8d720c9760308999e1acd4a7f99ff8f1c58841fd
DeepanshuManocha <51956189+DeepanshuManocha@users.noreply.github.com>
2020-09-26 23:43:22 +0530

    Added: Choose filer feature, UISet up

---
+++ Assets/Scripts/SelectFilter.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectFilter : MonoBehaviour
+{
+    public MeshRenderer meshRenderer;
+    
+    public void ChooseFiler (Material material)
+    {
+        meshRenderer.material = material;
+    }
+}
