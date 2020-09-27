bbbe03f2294469a362a253eba095ba0a5499eb2a
DeepanshuManocha <51956189+DeepanshuManocha@users.noreply.github.com>
2020-09-27 18:13:03 +0530

    Added: ScreenShot feature

---
+++ Assets/Scripts/ScreenShot.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenShot : MonoBehaviour
+{
+    private string fileName= "Screenshot"+ System.DateTime.Now.Hour + System.DateTime.Now.Minute + System.DateTime.Now.Second + ".png";
+    private string filePath;
+    void Start()
+    {
+        filePath = Application.persistentDataPath + "/";
+        if (!System.IO.Directory.Exists(filePath))
+        {
+            System.IO.Directory.CreateDirectory(filePath);
+        }
+    }
+
+   public void ScreenShotFunction()
+    {
+        ScreenCapture.CaptureScreenshot(fileName);
+        System.IO.File.Move(fileName, filePath);
+    }
+}
